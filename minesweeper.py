3f3396d3c1dc11d10584d27b4beb3548d15fb024
Connor Christian <connorjchristian@gmail.com>
2017-07-28 19:05:27 -0400

    Basic initialization and print functionality added
    Four functions have been added as starting point:
        * shuffle()
        * set_index()
        * get_index()
        * display()
    
    set/get_index are helper functions to translate the one dimensional list
    into two dimensions.

---
+++ minesweeper.py
@@ -0,0 +1,36 @@
+#!/usr/bin/env python
+import sys, random
+
+#default settings
+mines = 10 
+rows = columns = 8
+
+def shuffle():
+    # Returns a randomized board with the settings above
+    grid = ['m']*mines
+    grid.extend(['_']*(rows*columns -mines))
+    random.shuffle(grid)
+    
+    return grid
+
+def set_index(grid, x, y, value):
+    grid[x+(y*columns)] = value
+
+def get_index(grid, x, y):
+    return grid[x+(y*columns)]
+
+def display(grid):
+    print ' ',
+    for c in range(columns):
+        print '  ' + str(c),
+    print '\n  '+"----"*columns + '-'
+    
+    for r in range(rows):
+        print r,
+        for c in range(columns):
+            print "| " + get_index(grid,r,c),
+        print "|\n  " + "----"*columns + '-'
+
+if __name__ == "__main__":
+    board = shuffle()
+    display(board)
